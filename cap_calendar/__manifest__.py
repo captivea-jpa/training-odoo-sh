2f2267fa12045a2ee4d99d75aeb68544e52af5d5
captivea-jpa <joffrey.paret@captivea.fr>
2018-10-20 13:17:59 +0200

    #TEST: rename main 'calendar' menu to 'Appointments'.

--- cap_calendar/__manifest__.py
+++ cap_calendar/__manifest__.py
@@ -4,9 +4,11 @@
 {
     'name': 'cap_calendar',
     'author': 'captivea-jpa',
+    'version': '0.1',
     'description': """
     Manage calendar module for testing odoo.sh.
     """,
     'depends': ['calendar'],
+    'data': ['views/calendar.xml'],
     'installable': True
 }
