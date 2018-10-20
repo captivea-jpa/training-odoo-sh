30f38a8bf22dc6b63f183341bd4599f8dc0e7a85
captivea-jpa <joffrey.paret@captivea.fr>
2018-10-20 12:52:53 +0200

    #TEST: add new 'cap_calendar' module.

---
+++ cap_calendar/__manifest__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 11.
+
+{
+    'name': 'cap_calendar',
+    'author': 'captivea-jpa',
+    'description': """
+    Manage calendar module for testing odoo.sh.
+    """,
+    'depends': ['calendar'],
+    'installable': True
+}
